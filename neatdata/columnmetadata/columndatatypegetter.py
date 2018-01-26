bd528a30a66cb641651780fe3f238a3b1de9585f
Peter-32 <peter@impactradius.com>
2018-01-26 07:37:39 -0800

    _

--- neatdata/columnmetadata/columndatatypegetter.py
+++ neatdata/columnmetadata/columndatatypegetter.py
@@ -1,16 +1,13 @@
 class ColumnDataTypeGetter:
+
     def __init__(self):
         pass
 
     def execute(self, trainX, indexColumns, skipColumns):
-        columns = self.trainX.columns.values.tolist()
-        numberColumns = []
-        categoryColumns = []
-        datetimeColumns = []
+        numberColumns, categoryColumns, datetimeColumns, columns = [], [], [], trainX.columns.values.tolist()
         for column in columns:
             datatype = trainX[column].dtype
-            if column in indexColumns or column in skipColumns:
-                continue
+            if column in indexColumns or column in skipColumns: pass
             elif datatype == 'int64' or datatype == 'float64':
                 numberColumns.append(column)
             elif datatype == 'object':
